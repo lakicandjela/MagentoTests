27498712d1fe801f51634593f6795bba31fb0bf5
“Andjela” <lakic.andjela@gmail.com>
2024-04-23 11:07:09 +0200

    Created suit for all tests that need to go to sign in first

--- e2e/tests.spec.ts
+++ e2e/tests.spec.ts
@@ -8,35 +8,6 @@ test.beforeEach( async ({page}) => {
 	await page.goto('/')
 })
 
-test('Removing item completely from the cart in the cart menu', async({page}) => {
-    // Setup:
-    const pm = new PageManager(page)  // Create a page manager for interactions
-    await pm.navigateTo().signInPage() // Navigate to sign-in
-    pm.onSignInPage().signInWithCredentials(exsistingUser.email, exsistingUser.password) // Sign in
-
-    // Record initial cart counter value
-    const counter_before = await page.locator('.counter-number').textContent()
-
-    // Add product using helper function
-    await pm.fromHelperBase().chooseProductWithSizeAndColor(product1.code, product1.size, product1.color)
-
-    // Verify product added by checking cart counter change
-    await expect(async () => {
-		await expect(await page.locator('.counter-number').textContent()).not.toEqual(counter_before);
-	}).toPass();
-
-    // Open the cart menu
-    await page.locator('[class="minicart-wrapper"]').click()
-
-    // Click 'Remove' button
-    await page.getByText('Remove').click()
-
-    await page.locator('[class="action-primary action-accept"]').click()
-
-    // Verify the cart is empty
-    await expect(page.getByText('You have no items in your')).toBeVisible()
-
-})
 
 test('Error message is there while registering with existing email', async({page}) => {
     // Setup:
@@ -59,19 +30,53 @@ test('Error message is there while registering with existing email', async({page
     await expect(page.getByText('There is already')).toBeVisible();
 });
 
-test('Error message while log in with incorrect credentials', async({page}) => {
-        // Setup:
-        const pm = new PageManager(page); // Create a PageManager for page interactions
-        await pm.navigateTo().signInPage()
 
+test.describe('Go to log in page first', () => {
+    test.beforeEach( async ({page}) => {
+        // Setup:
+        const pm = new PageManager(page)  // Create a page manager for interactions
+        await pm.navigateTo().signInPage() // Navigate to sign-in
+    })
+
+    test('Removing item completely from the cart in the cart menu', async({page}) => {
+        const pm = new PageManager(page)  // Create a page manager for interactions
+        pm.onSignInPage().signInWithCredentials(exsistingUser.email, exsistingUser.password) // Sign in
+    
+        // Record initial cart counter value
+        const counter_before = await page.locator('.counter-number').textContent()
+    
+        // Add product using helper function
+        await pm.fromHelperBase().chooseProductWithSizeAndColor(product1.code, product1.size, product1.color)
+    
+        // Verify product added by checking cart counter change
+        await expect(async () => {
+            await expect(await page.locator('.counter-number').textContent()).not.toEqual(counter_before);
+        }).toPass();
+    
+        // Open the cart menu
+        await page.locator('[class="minicart-wrapper"]').click()
+    
+        // Click 'Remove' button
+        await page.getByText('Remove').click()
+    
+        await page.locator('[class="action-primary action-accept"]').click()
+    
+        // Verify the cart is empty
+        await expect(page.getByText('You have no items in your')).toBeVisible()
+    
+    })
+
+    test('Error message while log in with incorrect credentials', async({page}) => {
+        const pm = new PageManager(page)  // Create a page manager for interactions
         // Generate a random user (note: this assumes the helper function does NOT use the existing user's email)
         const user = pm.fromHelperBase().genRandomUser(); // Navigate to sign-in
 
         await pm.onSignInPage().signInWithCredentials((await user).email, (await user).password) // Sign in
 
         // Assert that the expected error message is displayed 
-        await expect(page.getByText('The account sign-in was')).toBeVisible();
-        
+        await expect(page.getByText('The account sign-in was')).toBeVisible();      
+    })
+
 })
 
 
