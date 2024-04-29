aed55a0de8b14ddeb0da4ae2c449e3044adf0c29
“Andjela” <lakic.andjela@gmail.com>
2024-04-29 09:04:08 +0200

    Fixed errors

--- e2e/QA9-111a.spec.ts
+++ e2e/QA9-111a.spec.ts
@@ -7,39 +7,39 @@ let pm: PageManager
 test.beforeEach(async ({ page }) => {
     pm = new PageManager(page);  // Create a page manager for interactions
     await page.goto('/')
-    await expect(pm.onHomepage().storeLogo).toBeVisible() // Assert that the site is opened
+    await expect(pm.onHomePage().storeLogo).toBeVisible() // Assert that the site is opened
 })
 
-test('Removing item completely from the cart in the cart menu', async ({ page }) => {
+test('Verify that an item can be completely removed from the cart in the cart menu', async ({ page }) => {
     // Setup:
     await pm.navigateTo().signInPage() // Navigate to sign-in
     await expect(pm.onSignInPage().loginPageTitle).toBeVisible()
 
     await pm.onSignInPage().signInWithCredentials(existingUser.email, existingUser.password) // Sign in
-    expect(await pm.onHomepage().openUserMenu).toBeVisible() // Assert that the user is successfuly signed in
+    expect(await pm.onHomePage().openUserMenu).toBeVisible() // Assert that the user is successfuly signed in
 
     // Record initial cart counter value
-    const counterBefore = await pm.onHomepage().cartCounterLocator.textContent()
+    const counterBefore = await pm.onHomePage().cartCounterLocator.textContent()
 
     // Add product 
     await pm.fromHelperBase().chooseProductWithSizeAndColor(product1.code, product1.size, product1.color)
 
     // Verify product added by checking cart counter change
     await expect(async () => {
-        expect(await pm.onHomepage().cartCounterLocator.textContent()).not.toEqual(counterBefore);
+        expect(await pm.onHomePage().cartCounterLocator.textContent()).not.toEqual(counterBefore);
     }).toPass();
 
     // Open the cart menu
-    await pm.onHomepage().minicartButton.click()
-    expect(pm.onHomepage().closeMinicartButton).toBeVisible()
+    await pm.onHomePage().minicartButton.click()
+    expect(pm.onHomePage().closeMinicartButton).toBeVisible()
 
     // Click 'Remove' button
-    await pm.onHomepage().removeFromCartButton.click()
-    expect(pm.onHomepage().removeFromCartPopupQuestionTitle).toBeVisible()
+    await pm.onHomePage().removeFromCartButton.click()
+    expect(pm.onHomePage().removeFromCartPopupQuestionTitle).toBeVisible()
 
     // Approve removal from cart
-    await pm.onHomepage().approveRemovalFromCartButton.click()
+    await pm.onHomePage().approveRemovalFromCartButton.click()
 
     // Final assertion: verify the cart is empty
-    await expect(pm.onHomepage().cartIsEmptyText).toBeVisible()
+    await expect(pm.onHomePage().cartIsEmptyText).toBeVisible()
 })
--- e2e/testQA9-104.spec.ts
+++ e2e/testQA9-104.spec.ts
@@ -1,19 +1,19 @@
 import { test, expect } from "@playwright/test"
 import { PageManager } from "../page-objects/pageManager"
-import { existingUser, product1, review1 } from "../helper/data"
+import { product1, review1 } from "../helper/data"
 
 let pm: PageManager
 
 test.beforeEach(async ({ page }) => {
     pm = new PageManager(page)   // Create a page manager for interactions
     await page.goto('/')
-    await expect(pm.onHomepage().storeLogo).toBeVisible() // Assert that the site is opened
+    await expect(pm.onHomePage().storeLogo).toBeVisible() // Assert that the site is opened
 
     // Sign in
     await pm.onSignInPage().goToLoginPageAndLoginWithExistingUser(pm)
 })
 
-test('Leave a review', async ({ page }) => {
+test('Verify that a product review could be made', async ({ page }) => {
     // Navigate to the product details page:
     await pm.fromHelperBase().clickOnProduct(product1.code)
 
@@ -21,5 +21,5 @@ test('Leave a review', async ({ page }) => {
     await pm.fromHelperBase().fillReview(review1)
 
     // Verify review submission success:
-    await expect(pm.onHomepage().successfulReviewMessage).toBeVisible()
+    await expect(await pm.onHomePage().successfulReviewMessage).toBeVisible()
 })
